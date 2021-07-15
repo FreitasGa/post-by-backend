39df3f543c4d474a0b8dd6c377ea6a08eb2bfc73
FreitasGa <freitasga.01@gmail.com>
2021-07-14 22:41:07 -0300

    feat: changes to email

--- src/functions/email/send-email.js
+++ src/functions/email/send-email.js
@@ -2,37 +2,40 @@ import handler from '../../../libs/handler-lib';
 import nodemailer from 'nodemailer';
 
 export const main = handler(async (event, context) => {
-    const { userEmail } =
-        typeof event.body === 'string' ? JSON.parse(event.body) : event.body;
+  const { userEmail } =
+    typeof event.body === 'string' ? JSON.parse(event.body) : event.body;
 
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'postby.lawgile@gmail.com',
-            pass: 'postby@123',
-        },
-        tls: {
-            rejectUnauthorized: false,
-        },
-    });
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'postby.lawgile@gmail.com',
+      pass: 'postby@123',
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
 
-    const htmlToSend = `
-        <h1>Nova reserva feita</h1>
-        <br />
-        <b>Vim aqui confirmar que sua reserva foi feita</b>
+  const htmlToSend = `
+        <center>
+            <h2>Nova reserva feita</h2>
+            <p style="margin:0" >Opa, tudo bem?</p>
+            <p style="margin:0" >Vim aqui confirmar que sua reserva foi feita!</p>
+            <img style="margin:20px 12px" width="248" src="https://i.giphy.com/media/tIeCLkB8geYtW/giphy.gif">
+        </center>
     `;
 
-    const emailSent = await transporter.sendMail({
-        from: 'Post-by <postby.lawgile@gmail.com>',
-        to: userEmail,
-        subject: 'Nova reserva!',
-        text: 'Nova reserva realizada',
-        html: htmlToSend,
-    });
+  const emailSent = await transporter.sendMail({
+    from: 'Post-by <postby.lawgile@gmail.com>',
+    to: userEmail,
+    subject: 'Nova reserva!',
+    text: 'Nova reserva realizada',
+    html: htmlToSend,
+  });
 
-    if (!emailSent.messageId) {
-        throw new Error('Falha no envio');
-    }
+  if (!emailSent.messageId) {
+    throw new Error('Falha no envio');
+  }
 
-    return 'Email enviado com sucesso!';
+  return 'Email enviado com sucesso!';
 });
